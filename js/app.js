c9f1506c81882b9f25790dff253eb29d56bf9348
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2023-04-13 16:54:24 +0300

    changed animation to infinity sliding

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 
+let slidesQuantity = 3;
+let actualSlide = 1;
+let previousSlide = slidesQuantity;
+let isSlideAnimating = false;
+
 const refs = {
     modalEnglish: document.querySelector('.modalEnglish'),
     modalEnglishBackdrop: document.querySelector('.modalEnglish__Backdrop'),
@@ -14,15 +19,16 @@ showModalEnglish.pause();
 showModalEnglish.to(refs.modalEnglishBackdrop, 0.6, { y: '-100%'});
 showModalEnglish.to(refs.modalEnglish, 0.6, { scale: 1, x:'-50%', y:'-50%'});
 
-var changeCerificates = gsap.timeline();
-changeCerificates.pause();
-changeCerificates.to(refs.certificatesList, 0.6, { x: '-33.33%'});
-changeCerificates.addPause();
-changeCerificates.to(refs.certificatesList, 0.6, { x: '-66.66%'});
+// var changeCerificates = gsap.timeline();
+// changeCerificates.pause();
+// changeCerificates.to(refs.certificatesList, 0.6, { x: '-33.33%'});
+// changeCerificates.addPause();
+// changeCerificates.to(refs.certificatesList, 0.6, { x: '-66.66%'});
 
 refs.modalEnglishOpenBtn.addEventListener('click', onModalEnglishOpenBtnClick)
 
 function onModalEnglishOpenBtnClick() {
+    firstSlidePosition()
     showModalEnglish.play();
     refs.modalEnglishCloseBtn.addEventListener('click', onModalEnglishCloseBtnClick)
     refs.modalEnglishBackdrop.addEventListener('click', onModalEnglishBackdropClick)
@@ -31,19 +37,22 @@ function onModalEnglishOpenBtnClick() {
     window.addEventListener('keydown', onKeydown)
 }
 function onModalEnglishCloseBtnClick() {
-    showModalEnglish.reverse();
+  showModalEnglish.reverse();
 }
 function onModalEnglishBackdropClick(event) {
-    if (!event.target.classList.contains('modalEnglish__Backdrop')) { return }
-    showModalEnglish.reverse();
+  if (!event.target.classList.contains('modalEnglish__Backdrop')) { return }
+  showModalEnglish.reverse();
   }
 function onModalEnglishLeftBtnClick() {
-    changeCerificates.reverse();
+  changeSlideLeft();
 }
 function onModalEnglishRightBtnClick() {
-    changeCerificates.play();
+  changeSlideRight();
 }
 function onKeydown(event) {
+  if (event.code === 'Escape') {
+    showModalEnglish.reverse();
+  }
   if (event.code === 'ArrowRight') {
     onArrowRightKeydown()
   }
@@ -52,8 +61,58 @@ function onKeydown(event) {
   }
 }
 function onArrowLeftKeydown() {
-    changeCerificates.reverse();
+  changeSlideLeft();
   }
 function onArrowRightKeydown() {
-    changeCerificates.play();
+  changeSlideRight();
+}
+
+
+
+function changeSlideRight() {
+if (isSlideAnimating) {return}
+actualSlide++;
+if (actualSlide <= slidesQuantity) {
+  previousSlide = actualSlide - 1;
+} else {
+  actualSlide = 1;
+  previousSlide = slidesQuantity;
+}
+animatingSlideRight()
+}
+
+function changeSlideLeft() {
+  if (isSlideAnimating) {return}
+  actualSlide--;
+  if (actualSlide === 0) {
+    actualSlide = slidesQuantity;
+    previousSlide = 1;
+  } else {
+    previousSlide = actualSlide + 1;
+  }
+  animatingSlideLeft()
+}
+
+function firstSlidePosition() {
+  let actualSlideObject = document.querySelector(`.certificatesList__item-${actualSlide}`);
+  var slideAnimation = gsap.timeline();
+  slideAnimation.fromTo(actualSlideObject, { x: '0' }, { duration: 0, x: '-100%'})
+}
+
+function animatingSlideRight() {
+  let actualSlideObject = document.querySelector(`.certificatesList__item-${actualSlide}`);
+  let previousSlideObject = document.querySelector(`.certificatesList__item-${previousSlide}`);
+
+  var slideAnimation = gsap.timeline();
+  slideAnimation.fromTo(actualSlideObject, { x: '0' }, { duration: 1, x: '-100%'})
+  .fromTo(previousSlideObject, { x: '-100%' }, { duration: 1, x: '-200%'}, "-=1")
+}
+
+function animatingSlideLeft() {
+  let actualSlideObject = document.querySelector(`.certificatesList__item-${actualSlide}`);
+  let previousSlideObject = document.querySelector(`.certificatesList__item-${previousSlide}`);
+
+  var slideAnimation = gsap.timeline();
+  slideAnimation.fromTo(actualSlideObject, { x: '-200%' }, { duration: 1, x: '-100%' })
+  .fromTo(previousSlideObject, { x: '-100%' }, { duration: 1, x: '0'}, "-=1")
 }
